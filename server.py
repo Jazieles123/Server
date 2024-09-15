c6776710c93168e47b4bc16a4d03347e7450316f
Jazieles123 <jazielrivas123@gmail.com>
2024-09-15 23:10:41 +0000

    Mundo guardado 2024-09-15

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from importlib import reload
 
 
-reloadreload# Versiones disponibles
+
+# Versiones disponibles
 # forge, mohist, fabric, vanilla, paper
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
